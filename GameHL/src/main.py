61179bc1c087e638dd6465168501d1a6eb5b3ae8
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-01-31 16:20:29 +0800

    Update main.py

--- GameHL/src/main.py
+++ GameHL/src/main.py
@@ -6,11 +6,9 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-bot = Bot(token="xxx")
+bot = Bot(token="1/MTM2OTk=/kqk/pdBPJwDb3DJv4SJfOA==") 
 
 print("机器人已成功启动")
-exp = {"root":"无限","test":999}
-userk = {"root":"237938831"}
 
 # prefixes=['召开']
 @bot.command(name="GameHL")
@@ -20,7 +18,7 @@ async def help(msg : Message): #注册/help命令函数
         Card(
             Module.Header('帮助 | GameHL'),
             Module.Divider(),
-            Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+            Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie/KOOK-BOT/blob/main/GameHL) 查看源码", type=Types.Text.KMD)),
             Module.Divider(),
             Module.Header('命令'),
             Module.Section('/GameHL : 获取帮助\n$注册 : 注册一个GHL账户\n$注销 : 注销一个GHL账号\n$经验 : 查询账号经验\n$抽奖 : 抽奖'),
@@ -30,9 +28,15 @@ async def help(msg : Message): #注册/help命令函数
     )
     await msg.ctx.channel.send(hep)
 
+# GHL账号系统
+
+exp = {"root":"无限"}
+userk = {"root":"237938831"}
+
 @bot.command(name="注册",prefixes="$")
 async def reg(msg : Message,name):
     exp[name] = 0
+    userk[name] = str(msg.author.id)
     await msg.ctx.channel.send("注册成功")
 
 @bot.command(name="注销",prefixes="$")
@@ -40,10 +44,22 @@ async def zx(msg : Message,name):
     exp.pop(name)
     await msg.ctx.channel.send("注销成功")
 
+# 经验值系统
+
 @bot.command(name="增加经验",prefixes="$")
 async def reg(msg : Message,name):
     if str(msg.author.id) == userk['root']:
         exp[name] += 10
+        await msg.ctx.channel.send("修改成功")
+    elif str(msg.author.id) != userk['root']:
+        await msg.ctx.channel.send("修改失败,您没有ROOT权限")
+    else:
+        await msg.ctx.channel.send("修改失败,您没有ROOT权限")
+
+@bot.command(name="减少经验",prefixes="$")
+async def reg(msg : Message,name):
+    if str(msg.author.id) == userk['root']:
+        exp[name] -= 10
         await msg.ctx.channel.send("修改成功")
     elif str(msg.author.id) != userk['root']:
         await msg.ctx.channel.send("修改失败,您没有ROOT权限")
@@ -63,6 +79,79 @@ async def cexp(msg : Message,name):
         )
     )
     await msg.ctx.channel.send(hep)
+
+# 探索系统
+
+@bot.command(name="探索",prefixes="$")
+async def cexp(msg : Message,world,name,user):
+    suc = ["成功","失败"]
+    Success = random.choice(suc)
+    if world == "主世界":
+        if name == "橡木林":
+            if Success == "失败":
+                hep = CardMessage(
+                    Card(
+                        Module.Header('探索 | 主世界'+name+'地区'),
+                        Module.Divider(),
+                        Module.Section("玩家"+user+"探索橡木林"+Success),
+                        Module.Divider(),
+                        Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                        color='#5A3BD7'
+                    )
+                )
+            elif Success == "成功":
+                sl = random.randint(1,64)
+                hep = CardMessage(
+                    Card(
+                        Module.Header('探索 | 主世界'+name+'地区'),
+                        Module.Divider(),
+                        Module.Section("玩家"+user+"探索橡木林"+Success+"\n获得了[树枝*"+str(sl)+"]"),
+                        Module.Divider(),
+                        Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                        color='#5A3BD7'
+                    )
+                )
+                exp[user] += 5
+        if name == "沙漠":
+            if exp[user] >= 10:
+                if Success == "失败":
+                    hep = CardMessage(
+                        Card(
+                            Module.Header('探索 | 主世界'+name+'地区'),
+                            Module.Divider(),
+                            Module.Section("玩家"+user+"探索沙漠"+Success),
+                            Module.Divider(),
+                            Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                            color='#5A3BD7'
+                        )
+                    )
+                elif Success == "成功":
+                    sl = random.randint(1,64)
+                    hep = CardMessage(
+                        Card(
+                            Module.Header('探索 | 主世界'+name+'地区'),
+                            Module.Divider(),
+                            Module.Section("玩家"+user+"探索沙漠"+Success+"\n获得了[沙子*"+str(sl)+"桶]"),
+                            Module.Divider(),
+                            Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                            color='#5A3BD7'
+                        )
+                    )
+                    exp[user] += 5
+            elif exp[user] < 10:
+                hep = CardMessage(
+                    Card(
+                        Module.Header('探索 | 主世界'+name+'地区'),
+                        Module.Divider(),
+                        Module.Section("玩家"+user+"探索沙漠失败,原因:经验值不足(>=10即可探索)"),
+                        Module.Divider(),
+                        Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                        color='#5A3BD7'
+                    )
+                )
+    await msg.ctx.channel.send(hep)
+
+# 抽奖系统
 
 @bot.command(name="抽奖",prefixes="$")
 async def hec(msg : Message): #注册/help命令函数
