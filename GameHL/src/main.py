4eb7d13b25aeec853b369432e990921f91856f40
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-02-02 14:05:28 +0800

    Update main.py

--- GameHL/src/main.py
+++ GameHL/src/main.py
@@ -4,7 +4,7 @@ import random
 import os
 import time
 
-bot = Bot(token="xxx") 
+bot = Bot(token="1/MTM2OTk=/kqk/pdBPJwDb3DJv4SJfOA==") 
 
 print("机器人已成功启动")
 
@@ -16,25 +16,88 @@ async def help(msg : Message): #注册/help命令函数
         Card(
             Module.Header('帮助 | GameHL'),
             Module.Divider(),
-            Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie/KOOK-BOT/blob/main/GameHL) 查看源码", type=Types.Text.KMD)),
+            Module.Context(Element.Text("由 (met)237938831(met) 开发,\
+                            在 [Github](https://github.com/Buelie/KOOK-BOT/blob/main/GameHL) 查看源码",\
+                            type=Types.Text.KMD)),
             Module.Divider(),
             Module.Header('命令'),
-            Module.Context(Element.Text("**`/GameHL` : 获取帮助\n`$注册` : 注册一个GHL账户\n`$注销` : 注销一个GHL账号\n`$经验` : 查询账号经验\n`$抽奖` : 抽奖**", type=Types.Text.KMD)),
-            Module.Section(''),
             Module.Divider(),
+                Module.Context(Element.Text("**`/GameHL` : 获取帮助\n`$注册` : 注册一个GHL账户\n`$注销` : 注销一个GHL账号\n`$经验` : 查询账号经验\n`$抽奖` : 抽奖**", type=Types.Text.KMD)),
+            Module.Divider(),
+            Module.Context(Element.Text("**GameHL V0.1.9更新\n* **`更新了探索机制`**\n  * **`探索成功率提高到90%,不再是一半`**\n  * **`绑定经验值系统`**\n  * **`经验值增加机制`**\n  * **`经验值访问机制`**\n  * **`增加下界维度及相关群系(荒地)`**\n* **`更新了ROOT权限`**\n* **`停止依赖第三方数据库`**", type=Types.Text.KMD)),
             color='#5A3BD7'
         )
     )
     await msg.ctx.channel.send(hep)
 
-# GHL账号系统
-
 exp = {"root":"无限"}
 userk = {"root":"237938831"}
+
+vip = {"A-On1":"237938831"}
+
+# 成就系统
+
+accomplishment_ExploreMadness_finish = {"1","2"}
+accomplishment_ExploreMadness = {"ROOT":15}
+
+@bot.command(name="激活成就",prefixes="$")
+async def reg(msg : Message,name,accomplishment,rot):
+    if accomplishment == "探索狂魔":
+        if rot == "权限激活":
+            if str(msg.author.id) == userk['root']:
+                accomplishment_ExploreMadness_finish.add(name)
+                hep = CardMessage(
+                    Card(
+                        Module.Header('成就 | 激活'),
+                        Module.Divider(),
+                        Module.Section("玩家"+name+"成就激活成功"),
+                        Module.Divider(),
+                        Module.Context(Element.Text("由 (met)237938831(met) 开发, 在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                        color='#5A3BD7'
+                    )
+                )
+            elif str(msg.author.id) != userk['root']:
+                hep = CardMessage(
+                    Card(
+                        Module.Header('成就 | 激活'),
+                        Module.Divider(),
+                        Module.Section("玩家"+name+"成就激活失败,原因:没有ROOT权限"),
+                        Module.Divider(),
+                        Module.Context(Element.Text("由 (met)237938831(met) 开发, 在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                        color='#fff000'
+                    )
+                )
+        elif rot == "激活":
+            if accomplishment_ExploreMadness[name] == 15:
+                hep = CardMessage(
+                    Card(
+                        Module.Header('成就 | 激活'),
+                        Module.Divider(),
+                        Module.Section("玩家"+name+"成就激活成功"),
+                        Module.Divider(),
+                        Module.Context(Element.Text("由 (met)237938831(met) 开发, 在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                        color='#5A3BD7'
+                    )
+                )
+            elif accomplishment_ExploreMadness[name] == 15:
+                hep = CardMessage(
+                    Card(
+                        Module.Header('成就 | 激活'),
+                        Module.Divider(),
+                        Module.Section("玩家"+name+"成就激活失败,原因:条件不满足"),
+                        Module.Divider(),
+                        Module.Context(Element.Text("由 (met)237938831(met) 开发, 在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                        color='#5A3BD7'
+                    )
+                )
+    await msg.ctx.channel.send(hep)
+
+# GHL账号系统
 
 @bot.command(name="注册",prefixes="$")
 async def reg(msg : Message,name):
     exp[name] = 0
+    accomplishment_ExploreMadness[name] = 0
     userk[name] = str(msg.author.id)
     await msg.ctx.channel.send("注册成功")
 
@@ -114,7 +177,7 @@ async def cexp(msg : Message,world,name,user):
                     )
                 )
                 exp[user] += 5
-        if name == "沙漠":
+        elif name == "沙漠":
             if exp[user] >= 10:
                 if Success == "失败":
                     hep = CardMessage(
@@ -151,6 +214,43 @@ async def cexp(msg : Message,world,name,user):
                         color='#5A3BD7'
                     )
                 )
+        elif name == "白桦林":
+            if exp[user] >= 10:
+                if Success == "失败":
+                    hep = CardMessage(
+                        Card(
+                            Module.Header('探索 | 主世界'+name+'地区'),
+                            Module.Divider(),
+                            Module.Section("玩家"+user+"探索白桦林"+Success),
+                            Module.Divider(),
+                            Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                            color='#5A3BD7'
+                        )
+                    )
+                elif Success == "成功":
+                    sl = random.randint(1,64)
+                    hep = CardMessage(
+                        Card(
+                            Module.Header('探索 | 主世界'+name+'地区'),
+                            Module.Divider(),
+                            Module.Section("玩家"+user+"探索白桦林"+Success+"\n获得了[白桦原木*"+str(sl)),
+                            Module.Divider(),
+                            Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                            color='#5A3BD7'
+                        )
+                    )
+                    exp[user] += 5
+            elif exp[user] < 10:
+                hep = CardMessage(
+                    Card(
+                        Module.Header('探索 | 主世界'+name+'地区'),
+                        Module.Divider(),
+                        Module.Section("玩家"+user+"探索白桦林失败,原因:经验值不足(经验值>=10即可探索)"),
+                        Module.Divider(),
+                        Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                        color='#5A3BD7'
+                    )
+                )
     elif world == "下界":
         if exp[user] >= 50:
             if name == "荒地":
@@ -178,12 +278,37 @@ async def cexp(msg : Message,world,name,user):
                         )
                     )
                     exp[user] += 5
+            if name == "地狱堡垒":
+                if Success == "失败":
+                    hep = CardMessage(
+                        Card(
+                            Module.Header('探索 | 下界'+name+'地区'),
+                            Module.Divider(),
+                            Module.Section("玩家"+user+"探索地狱堡垒"+Success),
+                            Module.Divider(),
+                            Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                            color='#5A3BD7'
+                        )
+                    )
+                elif Success == "成功":
+                    sl = random.randint(1,64)
+                    hep = CardMessage(
+                        Card(
+                            Module.Header('探索 | 下界'+name+'地区'),
+                            Module.Divider(),
+                            Module.Section("玩家"+user+"探索地狱堡垒"+Success+"\n获得了[下界犹*"+str(sl)),
+                            Module.Divider(),
+                            Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
+                            color='#5A3BD7'
+                        )
+                    )
+                    exp[user] += 5
         elif exp[user] < 50:
             hep = CardMessage(
                 Card(
                     Module.Header('探索 | 下界'+name+'地区'),
                     Module.Divider(),
-                    Module.Section("玩家"+user+"探索荒地失败,原因:经验值不足(经验值>=50即可探索)"),
+                    Module.Section("玩家"+user+"探索下界失败,原因:经验值不足(经验值>=50即可探索)"),
                     Module.Divider(),
                     Module.Context(Element.Text("由 (met)237938831(met) 开发,  在 [Github](https://github.com/Buelie) 查看源码", type=Types.Text.KMD)),
                     color='#5A3BD7'
